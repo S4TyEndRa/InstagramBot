650028a4c5e3428c161ac5025f38284a23e68c00
SaTyEndRa <84174959+S4TyEndRa@users.noreply.github.com>
2022-04-26 07:32:36 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -19,13 +19,13 @@ if ENVIRONMENT:
     INSTA_PASSWORD = os.environ.get('INSTA_PASSWORD', None)
 else:
     # Fill the Values
-    API_ID = 0
-    API_HASH = ""
-    BOT_TOKEN = ""
-    DATABASE_URL = ""
+    API_ID = 2171111
+    API_HASH = "fd7acd07303760c52dcc0ed8b2f73086"
+    BOT_TOKEN = "5263754363:AAFdmNpmqjZj3d0psXivJR11ni54mXmUWPU"
+    DATABASE_URL = "postgres://kmlgwxywypdwfn:e73e7eaabe3a49874e62b857bdc81ddf5294de73a7a18abc7733018194600651@ec2-79-125-59-247.eu-west-1.compute.amazonaws.com:5432/d83brciojnrd12"
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
-    MUST_JOIN = "StarkBots"
+    MUST_JOIN = "t24Talkies"
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN[1:]
-    INSTA_USERNAME = ""
-    INSTA_PASSWORD = ""
+    INSTA_USERNAME = "iam_insta_bot"
+    INSTA_PASSWORD = "Satyendra@8."
